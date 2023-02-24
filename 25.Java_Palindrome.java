152424a0b3a09b06abc3e04e26e5ef3c28deeb32
Vatsal Nanda <vatsalnanda3@gmail.com>
2023-02-25 01:29:39 +0530

    New file added

---
+++ 25.Java_Palindrome.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        
+       StringBuilder str = new StringBuilder(s);
+System.out.println((str.reverse().toString()).equals(s)?"Yes":"No"); 
+
+      
+        
+        
+        
+    }
+}
+
+
+
